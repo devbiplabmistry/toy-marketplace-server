7e9a3dfec0ac0ced64a17a969ed264c61c3c0f8b
devbiplabmistry <dev.biplabmistry@gmail.com>
2023-05-20 21:09:12 +0530

    env added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
-require('dotenv').config()
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
 const port = process.env.PORT || 5000
 
 // 
@@ -13,7 +13,7 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://edukit:beIE8UjXvPqu5lSY@cluster0.ovmmvr6.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ovmmvr6.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
