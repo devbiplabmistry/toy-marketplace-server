080626a2193645952025f9cf29a687c77b5a394a
devbiplabmistry <dev.biplabmistry@gmail.com>
2023-05-20 10:23:04 +0530

    all toys read methos added

--- index.js
+++ index.js
@@ -1,13 +1,82 @@
 const express = require('express')
 const app = express()
-const  cors = require('cors')
+const cors = require('cors')
 require('dotenv').config()
-const port = process.env.PORT ||5000
+const port = process.env.PORT || 5000
 
-
+// 
 
 // middleware
 app.use(cors())
+app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = "mongodb+srv://edukit:HPEqY4BGa9G24egv@cluster0.ovmmvr6.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("edukit");
+    const eduCollection = database.collection("eduProducts");
+
+    app.get('/toy', async (req, res) => {
+      const cursor = eduCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+    app.get('/toy/:id', async (req, res) => {
+      const id =req.params.id
+      console.log(id);
+      const query = { _id: new ObjectId(id) }
+      const user = await eduCollection.findOne(query)
+      res.send(user)
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Edukit website is running now...')
